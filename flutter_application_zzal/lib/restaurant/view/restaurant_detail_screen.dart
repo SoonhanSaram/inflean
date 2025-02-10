df69366f58a9bdfb84b0c06aa08538471630fa04
kkm <kyengmin911@naver.com>
2025-02-10 17:59:01 +0900

    sliver 작성

--- flutter_application_zzal/lib/restaurant/view/restaurant_detail_screen.dart
+++ flutter_application_zzal/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_zzal/common/layout/defalut_layout.dart';
+import 'package:flutter_application_zzal/product/component/product_card.dart';
 import 'package:flutter_application_zzal/restaurant/component/restaurant_card.dart';
 
 class RestaurantDetailScreen extends StatelessWidget {
@@ -11,24 +12,64 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: const [
-              '점츠',
-              '테스트',
-              '마스터',
-            ],
-            ratingsCount: 100,
-            deliveryTime: 30,
-            deliveryFee: 2000,
-            ratings: 4.5,
-            isDetail: true,
-            detail: '불타는 떡볶이는 맛있는 떡볶이 입니다.',
-          )
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLable(),
+          renderProducts(),
         ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: const [
+          '점츠',
+          '테스트',
+          '마스터',
+        ],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 2000,
+        ratings: 4.5,
+        isDetail: true,
+        detail: '불타는 떡볶이는 맛있는 떡볶이 입니다.',
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderLable() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
     );
   }
